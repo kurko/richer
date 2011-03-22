6e2e115efe4404861b29fcf1547ad7519abd20c8
Alexandre de Oliveira <chavedomundo@gmail.com>
2011-03-22 12:32:57 -0300

    Basic translation files created.

---
+++ translations/en.js
@@ -0,0 +1,18 @@
+/**
+ * en - English (International)
+ *
+ * You shouldn't need to use this file because it's built into richer.js. Use this
+ * only if you want to customize texts.
+ *
+ * @date March 2011
+ */
+var richertext = {
+	paragraph: 'Paragraph',
+	title: 'Title',
+
+	// formatting buttons
+	bold: 'Bold',
+	italic: 'Italic',
+	underline: 'Underline',
+	unorderedlist: 'List'
+}
---
+++ translations/pt_br.js
@@ -0,0 +1,16 @@
+/**
+ * pt_br - Portuguese (Brasil)
+ *
+ * @author Alexandre de Oliveira <chavedomundo@gmail.com>
+ * @date March 2011
+ */
+var richertext = {
+	paragraph: 'Parágrafo',
+	title: 'Título',
+
+	// formatting buttons
+	bold: 'Negrito',
+	italic: 'Itálico',
+	underline: 'Sublinhado',
+	unorderedlist: 'Lista'
+}
